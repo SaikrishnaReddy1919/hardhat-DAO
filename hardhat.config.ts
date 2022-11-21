76d4e0b526a82fbf18121d08434f9044145e28f4
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-11-21 14:46:26 +0530

    deploy script for governance token contract and delegating

---
+++ hardhat.config.ts
@@ -0,0 +1,34 @@
+import "hardhat-deploy";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat"
+import {HardhatUserConfig} from "hardhat/config"
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.8.9",
+            },
+        ],
+    },
+  networks: {
+    hardhat: {
+      chainId : 31337
+    },
+    localhost: {
+      chainId: 31337,
+    }
+    
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    }
+  }
+}
+export default config;
