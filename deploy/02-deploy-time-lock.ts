adf48e4592a48c842cda679ab8af8d04f272f492
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-11-21 15:04:05 +0530

    deploy script for timelock contract

---
+++ deploy/02-deploy-time-lock.ts
@@ -0,0 +1,27 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+//@ts-ignore
+import { ethers } from "hardhat";
+import { MIN_DELAY } from "../helper-hardhat-config";
+
+const deployTimeLock: DeployFunction = async function (
+    hre : HardhatRuntimeEnvironment
+) {
+//@ts-ignore
+    
+    const { getNamedAccounts, deployments } = hre;
+    const { deploy, log} = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("Deploying Time lock...")
+    const args = [MIN_DELAY, [], []]
+    const timeLockContract = await deploy("TimeLock",{
+        from : deployer,
+        args: args,
+        log: true,
+        // waitConfirmations : 1 //you need to wait for no. of blocks before verifying it on etherscan
+    })
+    //TODO: verify
+}
+ 
+export default deployTimeLock;
